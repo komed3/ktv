f77b9254eb7833ec97d861f04e6261d09fc006be
Paul Köhler <pk97@outlook.de>
2021-11-27 16:41:15 +0100

    add stream iframe

--- wp-content/plugins/ktv/src/plugins/stream.php
+++ wp-content/plugins/ktv/src/plugins/stream.php
@@ -2,7 +2,21 @@
 
     function __output_stream( $stream ) {
 
-        
+        $post = get_post( $stream->tv_id );
+
+        echo '<div class="stream">
+                <div class="video-container">
+                    <iframe class="video"
+                        src="https://www.youtube.com/embed/' . $stream->tv_stream . '"
+                        frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture"
+                        allowfullscreen></iframe>
+                </div>
+                <div class="info-container">
+                    <h1>' . get_the_title( $post ) . '</h1>
+                    <p>' . apply_filters( 'the_content', $post->post_content ) . '</p>
+                </div>
+            </div>
+        </div>';
 
     }
 
