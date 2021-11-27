32f797564f8a02f4f0fbbdf151ba56348a2f9e91
Paul Köhler <pk97@outlook.de>
2021-11-27 21:40:59 +0100

    add bg image for shedule events

--- wp-content/plugins/ktv/src/plugins/shedule.php
+++ wp-content/plugins/ktv/src/plugins/shedule.php
@@ -25,12 +25,17 @@
                 $terms[] = __get_live_link();
 
             $events[] = '<div class="event">
-                <div class="clock" time="' . ( time() - strtotime( $stream->tv_start ) ) . '"></div>
-                <div class="terms">
-                    ' . implode( '', $terms ) . '
+                ' . ( !empty( $stream->tv_stream )
+                    ? '<div class="image" style="background-image: url( https://i.ytimg.com/vi/' . $stream->tv_stream . '/hq720.jpg );"></div>'
+                    : '' ) . '
+                <div class="info">
+                    <div class="clock" time="' . ( time() - strtotime( $stream->tv_start ) ) . '"></div>
+                    <div class="terms">
+                        ' . implode( '', $terms ) . '
+                    </div>
+                    <h3>' . get_the_title( $post ) . '</h3>
+                    <p>' . implode( ' ', array_slice( explode( ' ', $post->post_content ), 0, 30 ) ) . ' &hellip;</p>
                 </div>
-                <h3>' . get_the_title( $post ) . '</h3>
-                <p>' . implode( ' ', array_slice( explode( ' ', $post->post_content ), 0, 30 ) ) . ' &hellip;</p>
             </div>';
 
         }
