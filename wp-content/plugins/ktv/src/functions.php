0565606ef3fc03e4cea025296219d454dc1587ab
komed3 <pk97@outlook.de>
2023-04-12 09:40:20 +0200

    restart project

--- wp-content/plugins/ktv/src/functions.php
+++ wp-content/plugins/ktv/src/functions.php
@@ -2,144 +2,30 @@
 
     require_once __DIR__ . '/admin/stream.php';
 
-    /*
+    /**
      * global vars
      * 
      */
 
+    define( 'WP_POST_REVISIONS', false );
+    define( 'AUTOSAVE_INTERVAL', 99999 );
+
     $ktvdb = $wpdb->prefix . 'ktv';
     $ktvdb_vers = '2.0.01';
 
-    /* 
-     * core functions
+    /**
+     * stream functions
      * 
      */
 
-    function __icon( bool $echo = false ) {
-
-        $icon = home_url( '/favicon.png' );
-
-        if( $echo ) echo esc_url( $icon );
-
-        return $icon;
-
-    }
-
-    function __credits( bool $echo = false ) {
-
-        $credits = sprintf(
-            __( 'Copyright &copy; %s by %s. Powered by komed3.', 'ktv' ),
-            wp_date( 'Y' ),
-            get_bloginfo( 'title' )
-        );
-
-        if( $echo ) echo $credits;
-
-        return $credits;
-
-    }
-
-    function __shortcodes( bool $echo = false ) {
-
-        $shortcodes = [];
-
-        foreach( [
-            'M' => __( 'Mute sound', 'ktv' ),
-            'K' => __( 'Play/Pause', 'ktv' ),
-            'F' => __( 'Fullscreen', 'ktv' ),
-            'ECS' => __( 'Close fullscreen', 'ktv' ),
-            '⯇' => __( '5 sec backwards', 'ktv' ),
-            '⯈' => __( '5 sec forwards', 'ktv' ),
-            '⯅' => __( 'Increase sound 5%', 'ktv' ),
-            '⯆' => __( 'Decrease sound 5%', 'ktv' )
-        ] as $key => $description ) {
-
-            $shortcodes[] = '<li>
-                <code>' . $key . '</code>
-                <span>' . $description . '</span>
-            </li>';
-
-        }
-
-        $content = '<h3>' . __( 'Keyboard shortcodes', 'ktv' ) . '</h3>
-        <ul class="shortcodes-list">
-            ' . implode( '', $shortcodes ) . '
-        </ul>';
-
-        if( $echo ) echo $content;
-
-        return $content;
-
-    }
-
-    function __active_stream() {
+    function __get_stream( int $ID ) {
 
         global $wpdb, $ktvdb;
 
         return $wpdb->get_row( '
             SELECT  *
             FROM    ' . $ktvdb . '
-            WHERE   tv_start < "' . date( 'Y-m-d H:i:s' ) . '"
-            AND     tv_end > "' . date( 'Y-m-d H:i:s' ) . '"
-        ' );
-
-    }
-
-    function __stream_terms( $stream, $post ) {
-
-        $terms = [];
-
-        foreach( [ 'channel', 'category' ] as $taxonomy ) {
-
-            foreach( get_the_terms( $post, $taxonomy ) as $term ) {
-
-                $terms[] = '<a href="' . get_term_link( $term, $taxonomy ) . '" class="term ' . $term->taxonomy . '">' . $term->name . '</a>';
-
-            }
-
-        }
-
-        if( !empty( $lang = $stream->tv_lang ) )
-            $terms[] = '<a href="#" class="term language">' . $lang . '</a>';
-
-        return $terms;
-
-    }
-
-    function __stream_tags( $stream, $post ) {
-
-        $tags = [];
-
-        foreach( get_the_tags( $post ) as $tag ) {
-
-            $tags[] = '<a href="' . get_tag_link( $tag ) . '" class="tag">' . $tag->name . '</a>';
-
-        }
-
-        return $tags;
-
-    }
-
-    function __use_plugin( string $plugin ) {
-
-        $path = __DIR__ . '/plugins/' . $plugin . '.php';
-
-        if( file_exists( $path ) )
-            require_once $path;
-        
-        else die( 'ERROR: plugin ' . $plugin . ' not found.' );
-
-    }
-
-    function __use_script(
-        string $script,
-        array $required = [ 'jquery' ]
-    ) {
-
-        wp_enqueue_script(
-            $script,
-            plugin_dir_url( __FILE__ ) . 'scripts/' . $script . '.js',
-            $required
+            WHERE   tv_id = ' . $ID
         );
 
     }
@@ -156,15 +42,26 @@
 
     }
 
-    function __get_stream( int $ID ) {
+    function __active_stream() {
 
         global $wpdb, $ktvdb;
 
         return $wpdb->get_row( '
             SELECT  *
             FROM    ' . $ktvdb . '
-            WHERE   tv_id = ' . $ID
-        );
+            WHERE   tv_start < "' . date( 'Y-m-d H:i:s' ) . '"
+            AND     tv_end > "' . date( 'Y-m-d H:i:s' ) . '"
+        ' );
+
+    }
+
+    function __stream_img( $stream ) {
+
+        return !empty( $stream->tv_stream )
+            ? '<div class="image" style="background-image: url( https://i.ytimg.com/vi/' .
+                  $stream->tv_stream .
+              '/hq720.jpg );"></div>'
+            : '';
 
     }
 
@@ -174,7 +71,7 @@
         string $lang,
         int $start,
         int $end,
-        bool $vod
+        bool $vod = false
     ) {
 
         global $wpdb, $ktvdb;
@@ -199,38 +96,18 @@
 
     }
 
-    function __stream_img( $stream ) {
-
-        return !empty( $stream->tv_stream )
-            ? '<div class="image" style="background-image: url( https://i.ytimg.com/vi/' . $stream->tv_stream . '/hq720.jpg );"></div>'
-            : '';
-
-    }
-
-    function __get_live_link() {
-
-        return '<a href="' . home_url( '/' ) . '" class="term live">' . __( 'Live', 'ktv' ) . '</a>';
-
-    }
-
-    function __get_vod_link( $post ) {
-
-        return '<a href="' . get_permalink( $post ) . '" class="term vod">' . __( 'VOD', 'ktv' ) . '</a>';
-
-    }
-
-    /* 
+    /**
      * initiate Komed TV plugin
      * 
      */
 
-    function __init() {
+    add_action( 'init', function () {
 
         # disable password recovery
 
         if( !is_admin() ) {
 
-          add_filter( 'allow_password_reset', '__return_false' );
+            add_filter( 'allow_password_reset', '__return_false' );
 
         }
 
@@ -244,16 +121,14 @@
 
         }
 
-    }
+    } );
 
-    add_action( 'init', '__init' );
-
-    /* 
+    /**
      * install / upgrade Komed TV plugin
      * 
      */
 
-    function __upgrade() {
+    add_action( 'plugins_loaded', function () {
 
         global $wpdb, $ktvdb, $ktvdb_vers;
 
@@ -281,8 +156,6 @@
 
         }
 
-    }
-
-    add_action( 'plugins_loaded', '__upgrade' );
+    } );
 
 ?>
